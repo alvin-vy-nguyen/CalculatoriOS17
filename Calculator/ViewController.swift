99be5c8425e9547c52ff57aa644341dffd96ac0f
Alvin Nguyen <alvin.nguyen@veeva.com>
2024-03-23 19:48:18 -0700

    Building on templete project by Angela Yu

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,19 +12,30 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    private var isFinishedTypingNumber: Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
+        
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        
+        if let numValue = sender.currentTitle {
+            // Shorter way of writing if true
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                // Looks at the button's current title and appends it to the current displayLabel text
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
     }
 
 }
